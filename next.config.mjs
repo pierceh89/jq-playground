85fe8e49fc62ec71c0c099c6a3ed6ed69bbcca34
Hyounggap.An <hyounggap.an@navercorp.com>
2025-07-02 08:18:45 +0900

    Fix: fixed github pages resource path issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+// GitHub Pages의 경우 저장소 이름을 basePath로 사용
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repoName =
+  process.env.GITHUB_REPOSITORY?.split("/")[1] || "jq-playground";
+
 const nextConfig = {
   output: "export",
+  basePath: isGithubActions ? `/${repoName}` : "",
+  assetPrefix: isGithubActions ? `/${repoName}/` : "",
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
